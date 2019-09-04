81b8305e634929e8906221dbdda127afd352b81d
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-09-04 22:05:19 +0200

    Changed zoom mechanism

--- example/js/main.js
+++ example/js/main.js
@@ -4,44 +4,43 @@ import './../../lib/sass/index.scss';
 var targetElement = document.querySelector('.example-holder');
 //TODO: Init fully
 var up360Interface = up360(targetElement, {
-    minZoom: 1,
-    maxZoom: 8,
+    fillWindow: true,
 
-    minFrame: 1,
-    maxFrame: 38,
+    minFrame: 0,
+    maxFrame: 359,
 
     playSpeed: 80,
     defaultPlayDirection: -1,
     rotationDivider: 10,
-
-    resourceUrl: "http://localhost:8000/chunks/scene/{index}_{column}_{row}_{totalColumns}_{totalRows}_{resX}_{resY}.jpg",
+    
+    resourceUrl: "http://172.21.66.11:8000/output/{index}_{column}_{row}_{totalColumns}_{totalRows}_{resX}_{resY}.jpg",
 
     levelObjects: [
         {
             zoomThreshold: 1,
-            width: 390,
-            height: 311,
+            width: 480,
+            height: 270,
             columns: 1,
             rows: 1
         },
         {
             zoomThreshold: 2,
-            width: 780,
-            height: 622,
+            width: 960,
+            height: 540,
             columns: 2,
             rows: 2
         },
         {
-            zoomThreshold: 4,
-            width: 1560,
-            height: 1246,
+            zoomThreshold: 3,
+            width: 1920,
+            height: 1080,
             columns: 4,
             rows: 4
         },
         {
-            zoomThreshold: 8,
-            width: 3121,
-            height: 2494,
+            zoomThreshold: 4,
+            width: 3840,
+            height: 2160,
             columns: 8,
             rows: 8
         }
